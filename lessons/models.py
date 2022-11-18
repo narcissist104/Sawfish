cb23105f820d5ef3f8c0aeb8d2ae0dfa01c307a2
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2022-11-18 21:23:00 +0000

    Created Student Database

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    # REQUIRED_FIELDS = ('name','email', 'password')
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
+    def __str__(self):
+        return str(self.name)
