f7296cf8eab3318949311baaf4815a1704c7db0a
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2022-11-18 21:29:59 +0000

    Added All Database objects

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student
+from .models import Student, Teacher, Director, Invoice
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'password')
+
+@admin.register(Teacher)
+class AdminAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'password')
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'password')
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoiceNum', 'referNum')
--- lessons/models.py
+++ lessons/models.py
@@ -2,9 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    # REQUIRED_FIELDS = ('name','email', 'password')
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
     password = models.CharField(max_length=20)
     def __str__(self):
         return str(self.name)
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
+    def __str__(self):
+        return str(self.name)
+
+class Director(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
+    def __str__(self):
+        return str(self.name)
+
+class Invoice(models.Model):
+    invoiceNum = models.CharField(max_length=30)
+    referNum = models.CharField(max_length=30)
+    def __str__(self):
+        return str(self.invoiceNum)
