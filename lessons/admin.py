870974c8ad62f6c9320f757bc20a53d6629b5771
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2022-11-22 11:04:05 +0000

    Finished adding databases

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student, Teacher, Director, Invoice
+from .models import Student, Admin, Director, Invoice, Lessons, Requests, Availability
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'password')
 
-@admin.register(Teacher)
+@admin.register(Admin)
 class AdminAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'password')
 
@@ -18,3 +18,15 @@ class DirectorAdmin(admin.ModelAdmin):
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
     list_display = ('invoiceNum', 'referNum')
+
+@admin.register(Lessons)
+class LessonsAdmin(admin.ModelAdmin):
+    list_display = ("title", "duration", "student_id", "teacher_id")
+
+@admin.register(Requests)
+class RequestsAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "duration", "interval", "topic", "teacher_id")
+
+@admin.register(Availability)
+class AvailabilityAdmin(admin.ModelAdmin):
+    list_display = ("request_id", "date")
--- lessons/models.py
+++ lessons/models.py
@@ -3,21 +3,21 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
+    email = models.EmailField()
     password = models.CharField(max_length=20)
     def __str__(self):
         return str(self.name)
 
-class Teacher(models.Model):
+class Admin(models.Model):
     name = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
+    email = models.EmailField()
     password = models.CharField(max_length=20)
     def __str__(self):
         return str(self.name)
 
 class Director(models.Model):
     name = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
+    email = models.EmailField()
     password = models.CharField(max_length=20)
     def __str__(self):
         return str(self.name)
@@ -27,3 +27,25 @@ class Invoice(models.Model):
     referNum = models.CharField(max_length=30)
     def __str__(self):
         return str(self.invoiceNum)
+
+class Lessons(models.Model):
+    title = models.CharField(max_length=20)
+    duration = models.IntegerField()
+    student_id = models.CharField(max_length=30)
+    teacher_id = models.CharField(max_length=30)
+    def __str__(self):
+        return str(self.title)
+
+class Requests(models.Model):
+    student_id = models.CharField(max_length=30)
+    # Store the availability as a reference to the dates within the availability database
+    duration = models.IntegerField()
+    # Interval stored as how many times per week
+    interval = models.IntegerField()
+    topic = models.CharField(max_length=20, default="")
+    teacher_id = models.CharField(max_length=30, default="")
+
+# Stores the dates in the availability database.
+class Availability(models.Model):
+    request_id = models.CharField(max_length=30, default="")
+    date = models.DateField()
