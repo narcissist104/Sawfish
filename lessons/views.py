1323cd9122e9c2f4854a50884f9edf7b9126d3f2
Matthew Klein <matthew-a-klein@github.com>
2022-11-23 10:27:01 +0000

    Add basic login and signup front end functionality

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import LogInForm, SignUpForm
+from django import forms
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('feed')
+    else:
+        form = SignUpForm()
+    return render(request, 'sign_up.html', {'form': form})
+
+def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('feed')
+        messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form})
