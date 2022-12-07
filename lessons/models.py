e36c230d89fec1ed1b4bd138489b008763b59589
OneebAamer <oneeb47@gmail.com>
2022-12-07 14:28:25 +0000

    Finished Admin Booking lessons and fixed teacher-request relationship

--- lessons/models.py
+++ lessons/models.py
@@ -86,5 +86,4 @@ class Request(models.Model):
     )
     duration = models.CharField(max_length=10, choices=SELVALUE_DURATION)
     booked = models.BooleanField(default=False)
-    teacher_id = models.IntegerField(max_length=20, choices=(()))
-    # teacher_id = models.CharField(max_length=20)
+    teacher_id = models.IntegerField(choices=(()))
