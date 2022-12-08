4612a59bb948cf8002a548189c4d3d9e27549d79
Narges R <89739799+narcissist104@users.noreply.github.com>
2022-12-08 14:10:27 +0000

    seeding requests and teachers

--- lessons/management/commands/seed.py
+++ lessons/management/commands/seed.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker 
-from lessons.models import User
+from lessons.models import User, Request
 from django.db import IntegrityError
+import datetime, random, teacher
 
 
 class Command(BaseCommand):
@@ -18,18 +19,39 @@ class Command(BaseCommand):
             print(f'Seeding user {user_count}',  end='\r')
             try:
                 self._create_user()
+                self._create_teacher()
             except (IntegrityError):
                 continue
             user_count += 1
         print('User seeding complete')
         
+    def _create_request(self, user):
+        for i in range(50):
+            student_id = user.id
+            instrument = self.faker.instrument()
+            availability = datetime.date(random.randint(2022, 2023), random.randint(1, 12), random.randint(1, 28))
+            number_of_lessons = random.randint(1, 5)
+            interval = random.randint(1, 5)
+            duration = random.randint(1, 5)
+            teacher_id = random.randint(1, self.COUNT)
+            new_request = Request.objects.create(
+                student_id,
+                instrument,
+                availability,
+                number_of_lessons,
+                interval,
+                duration,
+                teacher_id
+            )
+            new_request.save()
+
     def _create_user(self):
         first_name = self.faker.first_name()
         last_name = self.faker.last_name()
         email = self._email(first_name, last_name)
         username = self._username(first_name, last_name)
         bio = self.faker.text(max_nb_chars=520)
-        User.objects.create_user(
+        new_user = User.objects.create_user(
             username,
             first_name=first_name,
             last_name=last_name,
@@ -37,6 +59,14 @@ class Command(BaseCommand):
             password=Command.PASSWORD,
             bio=bio
         )
+        self._create_request(new_user)
+
+    def _create_teacher(self):
+        name = self.faker.name()
+        new_teacher = Teacher.objects.create_user(
+            name,
+        )
+        new_teacher.save()
 
     def _email(self, first_name, last_name):
         email = f'{first_name}.{last_name}@example.org'
@@ -45,5 +75,3 @@ class Command(BaseCommand):
     def _username(self, first_name, last_name):
         username = f'@{first_name}{last_name}'
         return username
-
-    
